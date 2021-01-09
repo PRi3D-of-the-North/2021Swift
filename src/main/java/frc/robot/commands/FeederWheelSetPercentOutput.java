40b4c7dad1f303ff1fee8484c5d2c610cc8cb7ba
Darren Seubert <dpseubert15@gmail.com>
2021-01-09 01:14:59 -0600

    Updated FeederWheelSetPercentOutput.java imports

--- src/main/java/frc/robot/commands/FeederWheelSetPercentOutput.java
+++ src/main/java/frc/robot/commands/FeederWheelSetPercentOutput.java
@@ -1,7 +1,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.FeederWheel;
 
 public class FeederWheelSetPercentOutput extends CommandBase {
